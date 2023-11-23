bc7b1a20f959bd82399bf75a7710168acdece318
Bhoomi Panchal <bhoomipanchal1821@gmail.com>
2023-11-23 15:51:53 +0530

    final-app

---
+++ src/Button.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from './Button.module.css';
+// import { styled } from "styled-components";
+
+// const Button = styled.button`
+//   width:100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #4CB9C8;
+//   color: white;
+//   background: #4CB9C8;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+// @media (min-width :768px){
+//   width:auto;
+// } 
+// }
+
+// &:focus {
+//   outline: none;
+// }
+
+// &button:hover,
+// &button:active {
+//   background:#5DCBB8;
+//   border-color:#DAF5F0;
+//   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+// }
+// `;
+
+const Button = props =>{
+  return (
+  <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
